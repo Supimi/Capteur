800fff1ffb105062f05011b78c124b2f44dcad47
supimi <supimi.15@cse.mrt.ac.lk>
2019-09-15 12:57:13 +0530

    minor changes

--- app/src/main/java/supimi/github/capteur/MainActivity.java
+++ app/src/main/java/supimi/github/capteur/MainActivity.java
@@ -10,11 +10,13 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
@@ -24,11 +26,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView out_x;
     private TextView out_y;
     private TextView out_z;
-    int i = 0;
 
-    private float x_reading = 0;
-    private float y_reading = 0;
-    private float z_reading = 0;
+    private double delta_x, delta_y, delta_z;
+    private double x_reading = 0;
+    private double y_reading = 0;
+    private double z_reading = 0;
 
     private SensorManager sensorManager;
     private Sensor accelerometer;
@@ -44,13 +46,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         this.out_x = findViewById(R.id.out_x);
         this.out_y = findViewById(R.id.out_y);
         this.out_z = findViewById(R.id.out_z);
+        this.set_initial_message();
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)!=null){
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
             accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        }else{
-
+        } else {
+            System.out.println("Sensor not found!!");
+            displayError();
         }
 
     }
@@ -79,6 +83,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void readSensorOutput(View view) {
         System.out.println("Start..................");
+        if(accelerometer==null){
+           displayError();
+        }
+        this.clean_readings();
         sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
@@ -89,11 +97,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
-        displayCurrentValue();
 
-        this.x_reading = sensorEvent.values[0];
-        this.y_reading = sensorEvent.values[1];
-        this.z_reading = sensorEvent.values[2];
+        //reading will change if sensor output changed by 1 decimal places
+        this.delta_x = Math.round(Math.abs(x_reading - sensorEvent.values[0]) * 10.0) / 10.0;
+        this.delta_y = Math.round(Math.abs(y_reading - sensorEvent.values[1]) * 10.0) / 10.0;
+        this.delta_z = Math.round(Math.abs(z_reading - sensorEvent.values[2]) * 10.0) / 10.0;
+
+
+        if (delta_x > 0.0) {
+            this.x_reading = sensorEvent.values[0];
+        }
+        if (delta_y > 0.0) {
+            this.y_reading = sensorEvent.values[1];
+        }
+
+        if (delta_z > 0.0) {
+            this.z_reading = sensorEvent.values[2];
+        }
+
+        this.displayCurrentValue();
     }
 
     @Override
@@ -102,8 +124,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
     public void displayCurrentValue() {
-        out_x.setText(Float.toString(x_reading));
-        out_y.setText(Float.toString(y_reading));
-        out_z.setText(Float.toString(z_reading));
+        out_x.setText(Double.toString(x_reading));
+        out_y.setText(Double.toString(y_reading));
+        out_z.setText(Double.toString(z_reading));
+    }
+
+    public void displayError() {
+        Context context = getApplicationContext();
+        CharSequence err_msg = "Sensor not found!";
+        int duration = Toast.LENGTH_LONG;
+
+        Toast toast = Toast.makeText(context, err_msg, duration);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER, 0, 900);
+        toast.show();
+    }
+
+    public void set_initial_message() {
+        String init_msg = "No readings yet...";
+        out_x.setText(init_msg);
+        out_y.setText(init_msg);
+        out_z.setText(init_msg);
+    }
+
+    public void clean_readings() {
+        String init_reading = "0.0";
+        out_x.setText(init_reading);
+        out_y.setText(init_reading);
+        out_z.setText(init_reading);
     }
 }
